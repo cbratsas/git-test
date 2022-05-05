8aa474f0866342563f56d851e4fcf3a5e4b3869d
cbratsas <cbratsas@math.auth.gr>
2022-05-05 13:41:32 +0300

    Revert "ggplot histogram"
    This reverts commit 448d0acf921f152a5a6ac6264b5022f9db901a4e.

--- script.R
+++ script.R
@@ -1,5 +1,3 @@
-library(ggplot2)
-
 # Load students dataset
 students_df <- read.csv("C:/git-test/data/students-git.csv")
 
@@ -8,9 +6,3 @@ summary(students_df)
 
 # View a histogram of the absences variable
 hist(students_df$absences)
-
-# View a histogram of the absences variable
-ggplot(students_df, aes(students_df$absences)) +
-  geom_histogram() +
-  labs(title = "Histogram for absences of Students") +
-  labs(x = "Absences", y = "Count")
