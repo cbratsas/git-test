d79bcf56f331260defb390dc3a3d47b26f755b9f
cbratsas <cbratsas@math.auth.gr>
2022-05-05 13:35:25 +0300

    Imported data and plotted a histogram

---
+++ script.R
@@ -0,0 +1,8 @@
+# Load students dataset
+students_df <- read.csv("C:/git-test/data/students-git.csv")
+
+# Look at a summary() of students3
+summary(students_df)
+
+# View a histogram of the absences variable
+hist(students_df$absences)
